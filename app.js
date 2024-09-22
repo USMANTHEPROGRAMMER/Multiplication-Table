7497749b1ef965849ebebff1e9f1541556c6c075
USMANTHEPROGRAMMER <zubaidakhatoon289@gmail.com>
2024-09-23 01:44:32 +0500

    Multiplication Table

---
+++ app.js
@@ -0,0 +1,12 @@
+var userInput = prompt("Write Multiplication table that you want (only numbers)");
+
+document.write(userInput + " " + "x" + " " + "1" + " = " + "1" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "2" + " = " + "2" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "3" + " = " + "3" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "4" + " = " + "4" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "5" + " = " + "5" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "6" + " = " + "6" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "7" + " = " + "7" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "8" + " = " + "8" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "9" + " = " + "9" * userInput + "<br>");
+document.write(userInput + " " + "x" + " " + "10" + " = " + "10" * userInput + "<br>");
